4fbd28838fae1bbd3cf5bee7f9d1f60b7f847fa2
ilyapetrenko <91661624+ilyapetrenko@users.noreply.github.com>
2023-03-19 05:44:36 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './index.css'
+import {Route, Routes} from "react-router-dom";
+import {HomePage} from "./pages/HomePage";
+import {FavouritesPage} from "./pages/FavouritesPage";
+import {Navigation} from "./components/Navigation";
+export const App = () => {
+  return (
+      <>
+          <Navigation/>
+    <Routes>
+      <Route path="/" element={<HomePage/>}/>
+      <Route path="/favourites" element={<FavouritesPage/>}/>
+    </Routes>
+      </>
+  )
+}
+
+export default App
